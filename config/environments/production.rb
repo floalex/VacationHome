492c8d8e225ef106b9e2e5f373c0809972c11ec7
floalex <flo.liang@my.com>
2015-11-03 23:17:14 -0800

    Add production site config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'vacation-home.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mandrillapp.com',
+    port: '587',
+    enable_starttls_auto: true,
+    user_name: ENV['mandrill_user_name'],
+    password: ENV['mandrill_password'],
+    authentication: 'login'.
+    domain: 'heroku.com'
+  }
 end
