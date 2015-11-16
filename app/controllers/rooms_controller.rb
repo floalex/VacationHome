24e6ff02d51a043c9c7923cef248e4c7e5cc4222
floalex <flo.liang@my.com>
2015-11-15 23:08:42 -0800

    Add room view

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,12 +1,14 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:show]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @rooms = current_user.rooms
   end
 
   def show
+    @photos = @room.photos
   end
 
   def new
@@ -17,18 +19,34 @@ class RoomsController < ApplicationController
     @room = current_user.rooms.build(room_params)
 
     if @room.save
-      redirect_to @room, notice: "Saved.."
+
+      if params[:images]
+        params[:images].each do |image|
+          @room.photos.create(image: image)
+        end
+      end
+      @photos = @room.photos
+      
+      redirect_to edit_room_path(@room), notice: "Saved.."
     else
       render :new
     end
   end
 
   def edit
+    @photos = @room.photos
   end
 
+
   def update
-    if @room.save
-      redirect_to @room, notice: "Updated.."
+    if @room.update(room_params)
+
+      if params[:images]
+        params[:images].each do |image|
+          @room.photos.create(image: image)
+        end
+      end
+      redirect_to edit_room_path(@room), notice: "Updated.."
     else
       render :edit
     end
@@ -40,8 +58,13 @@ class RoomsController < ApplicationController
     end
 
     def room_params
-      params.require(:room).permit(:home_type, :room_type, :acoommodate, :bed_room, :barh_room, :listing_name,
-                                   :summary, :address, :has_tv, :has_kitchen, :has_air, :has_heating, :internet,
+      params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name,
+                                   :summary, :address, :has_tv, :has_kitchen, :has_air, :has_heating, :has_internet,
                                    :price, :active)
+    end
+
+    def correct_user
+      current_user.id == @room.user.id
+      redirect_to root_url if @room.nil?
     end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -6,8 +6,9 @@ class Room < ActiveRecord::Base
   validates :room_type, presence: true
   validates :accommodate, presence: true
   validates :bed_room, presence: true
-  validates :bath_type, presence: true
-  validates :listing_name, presence: true, length: {maximum: 50}
-  validates :summary, presence: true, length: {maximum: 500}
+  validates :bath_room, presence: true
+  validates :listing_name, presence: true, length: { maximum: 50 }
+  validates :summary, presence: true, length: { maximum: 500 }
   validates :address, presence: true
+
 end
