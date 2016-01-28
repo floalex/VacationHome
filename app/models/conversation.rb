bf350b900588536759039012766138e678a07acd
floalex <flo.liang@my.com>
2016-01-28 01:04:44 -0800

    Add customer message view

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -7,11 +7,11 @@ class Conversation < ActiveRecord::Base
   validates_uniqueness_of :sender_id, scope: :recipient_id
 
   scope :involving, -> (user) do
-    where("conversations.sender_id = ? OR conversation.recipient_id = ?", user.id, user.id)
+    where("conversations.sender_id = ? OR conversations.recipient_id = ?", user.id, user.id)
   end
 
   scope :between, -> (sender_id, recipient_id) do
-    where("(conversation.sender_id = ? AND conversations.recipient_id = ?) OR (conversation.sender_id = ? AND conversations.recipient_id = ?)", 
+    where("(conversations.sender_id = ? AND conversations.recipient_id = ?) OR (conversations.sender_id = ? AND conversations.recipient_id = ?)", 
           sender_id, recipient_id, recipient_id, sender_id)
   end
 end
